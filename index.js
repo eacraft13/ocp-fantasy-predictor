935feb7c08a7ff71d20dc37bcbd5a7f01edbac54
Eric Craft <eacraft13@gmail.com>
2015-11-18 03:11:28 -0800

    Add commander for this to be a command line tool

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-var _           = require('lodash');
-var llCorrelate = require('./lib/league_leaders/correlate');
-var r           = require('rethinkdb');
-var rOptions    = require('./config/rethinkdb');
+#!/usr/bin/env node
+
+var _         = require('lodash');
+var correlate = require('./lib/correlate.js');
+var program   = require('commander');
+var r         = require('rethinkdb');
+var rOptions  = require('./config/rethinkdb.js');
 
 
+program
+.version('0.0.1')
+.option('-p --position <position>', 'Player\'s  position', /^(qb|rb|wr|te|k|d)$/i, 'qb')
+.option('-s --season   <season>',   'Player\s season',     /^(2015|2014|2013)$/i, '2015')
+.option('-t --table    <table>',    'Table to query',      /./i, 'player_game_stats_by_week')
+.parse(process.argv);
 
+console.log(program.position, program.season, program.table);
 
 r.connect(rOptions.connection, function(err, conn) {
     if (err) throw err;
 
     conn.use(rOptions.db);
 
-    r.table('season_league_leaders')
+    r.table(program.table)
     .filter({
-        Season: 2015,
-        Position: 'QB'
+        Season: program.season,
+        Position: program.position.toUpperCase()
     })
     .run(conn, function(err, cursor) {
         cursor.toArray(function(err, results) {
             if (err) throw err;
-            llCorrelate(results, function(err, data) {
+            correlate(results, function(err, data) {
                 var display = _.chain(data)
                     .filter(function(val) {
                         return val.corrcoeff;
