cd2806d5da38b29574c225e5187c200ce857bfb8
Eric Craft <eacraft13@gmail.com>
2015-11-21 00:10:25 -0800

    Update rethink configs

--- config/rethinkdb.js
+++ config/rethinkdb.js
@@ -1,7 +1,7 @@
 module.exports = {
-	connection: {
-		host: '45.56.91.5',
-		port: '28015'
-	},
-	db: 'nfl'
+    connection: {
+        host: '45.79.78.74',
+        port: '28015'
+    },
+    db: 'ocp_apim'
 };
